498c80bfcf7686d79e154b96cd25e9d4148c015e
Fabrizio <fabrizziotaco20@gmail.com>
2025-09-15 23:01:10 -0500

    refactor: permissions logic

--- backend/app/product/permissions.py
+++ backend/app/product/permissions.py
@@ -2,12 +2,6 @@ from utils.permissions_mixins import RoleActionPermissionMixin
 
 class ProductPermission(RoleActionPermissionMixin):
     ROLE_PERMISSIONS = {
-        "tester": ["create"], #Example
+        "tester": ["create"],
+        "anonymous": ["list", "retrieve"]
     }
-
-    def has_permission(self, request, view):
-        # Public read
-        if view.action in ["list", "retrieve"]:
-            return True
-
-        return super().has_permission(request, view)
--- backend/utils/permissions_mixins.py
+++ backend/utils/permissions_mixins.py
@@ -1,14 +1,15 @@
 from rest_framework.permissions import BasePermission
 
 class RoleActionPermissionMixin(BasePermission):
-
     allow_admin = True
 
     def has_permission(self, request, view):
-        role_name = getattr(request.user.role, "name", None)
-
-        if self.allow_admin and role_name == "admin":
-            return True
+        if not request.user.is_authenticated:
+            role_name = "anonymous"
+        else:
+            role_name = getattr(request.user.role, "name", None)
+            if self.allow_admin and role_name == "admin":
+                return True
 
         allowed_actions = getattr(self, "ROLE_PERMISSIONS", {}).get(role_name, [])
         return view.action in allowed_actions
