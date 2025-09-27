efff76c4ad2fda48c6d93d45cb7ca75ac3315a13
Fabrizio <fabrizziotaco20@gmail.com>
2025-09-26 21:13:54 -0500

    feat: update product interfaces to paginated and details

--- frontend/src/types/products.ts
+++ frontend/src/types/products.ts
@@ -1,3 +1,4 @@
+// Interfaces for quick product data
 export interface ImageData {
   image_url: string;
   alt_text: string;
@@ -11,8 +12,30 @@ export interface Product {
   images: ImageData[];
 }
 
-export interface ProductsData {
+export interface PaginatedProducts {
   count: number;
   num_pages: number;
-  productos: Product[];
+  products: Product[];
+}
+
+//Interfaces for detailed product information
+import { Tag } from "@/types/tags";
+import { Sport} from "@/types/sports";
+
+export interface ProductDetail {
+  id: string;
+  name: string;
+  price: number;
+  stock: number;
+  description: string;
+  slug: string;
+  weight: number;
+  status: string;
+  brand: string;
+  category: string;
+  series?: string;
+  character?: string;
+  tags: Tag[];
+  sports: Sport[];
+  images: ImageData[];
 }
